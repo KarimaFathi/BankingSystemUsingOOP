8963cba9d6e4f50d61570065f7cf2536e8646cf1
KarimaFathi <karimafathifadili@gmail.com>
2025-06-09 15:47:55 +0100

    Add login lockout after 3 failed attempts

--- bankingSystemWithOOP/bankingSystemWithOOP.cpp
+++ bankingSystemWithOOP/bankingSystemWithOOP.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main()
 {
     while (true) {
-        clsLoginScreen::showLoginScreen();
+        if (clsLoginScreen::showLoginScreen() == false) {
+            break;
+        }
     }
 }
 
--- bankingSystemWithOOP/clsLoginScreen.h
+++ bankingSystemWithOOP/clsLoginScreen.h
@@ -5,30 +5,41 @@
 #include "global.h"
 
 
-class clsLoginScreen : protected clsScreen 
+class clsLoginScreen : protected clsScreen
 {
 private:
-	static void _login() {
+	static bool _login() {
 		bool failedLogin = false;
 		string userName, password;
+		short failedTrials = 0;
 		do {
 			if (failedLogin == true) {
+				failedTrials++;
 				cout << "\nInvalid UserName/Password\n";
+				cout << "You have " << 3 - failedTrials << " trial(s) to login.";
 			}
-			cout << "\nEnter UserName : ";
+			if (failedTrials == 3) {
+				cout << "\n\nYou are locked after 3 failed trials ! \n";
+				return false;
+			}
+			cout << "\n\nEnter UserName : ";
 			userName = clsInputValidate::readString();
-			cout << "\nEnter password : ";
+			cout << "Enter password : ";
 			password = clsInputValidate::readString();
 			currentUser = clsUser::find(userName, password);
 			failedLogin = currentUser.isEmpty();
 		} while (failedLogin);
 		clsMainScreen::showMainMenu();
+		return true;
 	}
-public: 
-	static void showLoginScreen() {
+
+
+public:
+	static bool showLoginScreen() {
 		system("cls");
 		_drawScreenHeader("Login Screen");
-		_login();
+		return _login();
 	}
+
 };
 
